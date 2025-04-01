039c46a654cd019982fd35e93a4891e45bbb404b
Mugdha Sonare <66064234+mugdha268@users.noreply.github.com>
2025-04-01 18:32:58 +0000

    Initial Commit

---
+++ 585. Investments in 2016.sql
@@ -0,0 +1,14 @@
+select round(sum(tiv_2016),2) as tiv_2016 
+FROM Insurance 
+where tiv_2015 in (
+    select tiv_2015
+    from Insurance
+    group by 1
+    having count(*)>1
+) and concat (lat ,lon) in
+(
+    select concat ( lat , lon)
+    from Insurance
+    group by lat,lon 
+    having count(*) =1
+);
